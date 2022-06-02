e1b9405db9f69dc4acbcf43281c317b860e313a9
qiuzhanghua <qiuzhanghua@icloud.com>
2022-06-02 21:44:15 +0800

    format

--- src/App.svelte
+++ src/App.svelte
@@ -4,9 +4,9 @@
   import logo from "./assets/svelte.png";
   import Counter from "./lib/Counter.svelte";
   import { Button } from "@opentj/jui";
-  import Switch from "heroicons/solid/switch-horizontal.svg"
-  import {Icon} from '@steeze-ui/svelte-icon'
-  import {Annotation} from '@steeze-ui/heroicons'
+  import Switch from "heroicons/solid/switch-horizontal.svg";
+  import { Icon } from "@steeze-ui/svelte-icon";
+  import { Annotation } from "@steeze-ui/heroicons";
   // import {SwitchHorizontal} from '@steeze-ui/heroicons'
 
   const toggleDarkMode = function () {
@@ -20,7 +20,7 @@
 
 <main>
   <img src={logo} alt="Svelte Logo" />
-	<Counter />
+  <Counter />
   <div class="mt-4">Hello Tailwind CSS!</div>
   <Button
     class="
@@ -35,15 +35,15 @@
   >
     Toggle Light/Dark
     <Switch class="ml-3 mr-1 mt-[0.125rem] h-5 w-5" />
-<!--	  <Icon src={SwitchHorizontal} theme='solid' class='ml-3 mr-1 mt-[0.125rem] h-5 w-5'/>-->
+    <!--	  <Icon src={SwitchHorizontal} theme='solid' class='ml-3 mr-1 mt-[0.125rem] h-5 w-5'/>-->
   </Button>
 
-	<div class="flex justify-around">
-		<Icon src={Annotation} theme='solid' class='text-red-500' width={48}/>
-		<Icon src={Annotation} theme='default' class='text-red-500' width={48}/>
-	</div>
+  <div class="flex justify-around">
+    <Icon src={Annotation} theme="solid" class="text-red-500" width={48} />
+    <Icon src={Annotation} theme="default" class="text-red-500" width={48} />
+  </div>
 
-	<!--	https://tailwindcss-forms.vercel.app/-->
+  <!--	https://tailwindcss-forms.vercel.app/-->
   <!--	<input type="date" class="mt-1 block w-full">-->
 
   <!--	<div class="aspect-w-16 aspect-h-9">-->
